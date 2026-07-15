7ee823f87557efc5ad7c52d49a671bcd85b0ce05
piklz <pixelpiklz@gmail.com>
2026-07-15 17:12:19 +0100

    t

--- .templates/immich/meta.sh
+++ .templates/immich/meta.sh
@@ -2,7 +2,7 @@
 # .templates/immich/meta.sh
 SERVICE_DESC="Immich photo library"
 SERVICE_ICON="🖼️"
-SERVICE_ARCH="All" 
+SERVICE_ARCH="all" 
 SERVICE_TAGS="media" 
 SERVICE_DEPS="" 
 SERVICE_CONFIGS="" 
