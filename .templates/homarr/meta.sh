5f084d974324cd6f817f53892c6a9d86e72f46ef
piklz <pixelpiklz@gmail.com>
2026-07-18 16:04:36 +0100

    homarr upgrade to newer suppoerted version

--- .templates/homarr/meta.sh
+++ .templates/homarr/meta.sh
@@ -9,4 +9,4 @@ SERVICE_ICON="🏠"
 SERVICE_ARCH="all"
 SERVICE_TAGS="dashboard" 
 SERVICE_DEPS=""                 # Leave empty if none
-SERVICE_CONFIGS="config.yaml" # Example config file to be seeded
+SERVICE_CONFIGS=""              # Example config file to be seeded
