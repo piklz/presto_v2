e9b1038b22dec6ada035e13a37d10c43457ca67a
piklz <pixelpiklz@gmail.com>
2026-09-03 11:49:13 +0100

    adding nostalgia tv sched server  to test

---
+++ .templates/nostalgiatv/meta.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# .templates/nostalgiatv/meta.sh
+#
+# ─── Presto template metadata ───────────────────────────────────────────────
+# Sourced by presto during discovery and build.  NEVER deployed to services/. 
+#
+SERVICE_DESC="Nostalgiatv Media Server"
+SERVICE_ICON="🎬tv "
+SERVICE_ARCH="all"
+SERVICE_TAGS="media" 
+SERVICE_DEPS=""                 # Leave empty if none
+SERVICE_CONFIGS="" # Example config file to be seeded
